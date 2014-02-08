7669b3a3eb6c530a8a3007fe2511d413df8095ce
liqian <liqian@no5media.com>
2014-02-08 20:31:17 +0800

    init

---
+++ src/main/java/com/bruce/weixin/bean/WeatherInfo.java
@@ -0,0 +1,288 @@
+package com.bruce.weixin.bean;
+
+public class WeatherInfo {
+
+    private String city;
+    private String date_y;
+    private String week;
+    private String temp1;
+    private String temp2;
+    private String temp3;
+    private String temp4;
+    private String temp5;
+    private String temp6;
+    private String weather1;
+    private String weather2;
+    private String weather3;
+    private String weather4;
+    private String weather5;
+    private String weather6;
+
+    private String wind1;
+    private String wind2;
+    private String wind3;
+    private String wind4;
+    private String wind5;
+    private String wind6;
+
+    private String fl1;
+    private String fl2;
+    private String fl3;
+    private String fl4;
+    private String fl5;
+    private String fl6;
+
+    private String index_d;
+    private String index48_d;
+    private String index_uv;
+    private String index48_uv;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDate_y() {
+        return date_y;
+    }
+
+    public void setDate_y(String date_y) {
+        this.date_y = date_y;
+    }
+
+    public String getWeek() {
+        return week;
+    }
+
+    public void setWeek(String week) {
+        this.week = week;
+    }
+
+    public String getTemp1() {
+        return temp1;
+    }
+
+    public void setTemp1(String temp1) {
+        this.temp1 = temp1;
+    }
+
+    public String getTemp2() {
+        return temp2;
+    }
+
+    public void setTemp2(String temp2) {
+        this.temp2 = temp2;
+    }
+
+    public String getTemp3() {
+        return temp3;
+    }
+
+    public void setTemp3(String temp3) {
+        this.temp3 = temp3;
+    }
+
+    public String getTemp4() {
+        return temp4;
+    }
+
+    public void setTemp4(String temp4) {
+        this.temp4 = temp4;
+    }
+
+    public String getTemp5() {
+        return temp5;
+    }
+
+    public void setTemp5(String temp5) {
+        this.temp5 = temp5;
+    }
+
+    public String getTemp6() {
+        return temp6;
+    }
+
+    public void setTemp6(String temp6) {
+        this.temp6 = temp6;
+    }
+
+    public String getWeather1() {
+        return weather1;
+    }
+
+    public void setWeather1(String weather1) {
+        this.weather1 = weather1;
+    }
+
+    public String getWeather2() {
+        return weather2;
+    }
+
+    public void setWeather2(String weather2) {
+        this.weather2 = weather2;
+    }
+
+    public String getWeather3() {
+        return weather3;
+    }
+
+    public void setWeather3(String weather3) {
+        this.weather3 = weather3;
+    }
+
+    public String getWeather4() {
+        return weather4;
+    }
+
+    public void setWeather4(String weather4) {
+        this.weather4 = weather4;
+    }
+
+    public String getWeather5() {
+        return weather5;
+    }
+
+    public void setWeather5(String weather5) {
+        this.weather5 = weather5;
+    }
+
+    public String getWeather6() {
+        return weather6;
+    }
+
+    public void setWeather6(String weather6) {
+        this.weather6 = weather6;
+    }
+
+    public String getWind1() {
+        return wind1;
+    }
+
+    public void setWind1(String wind1) {
+        this.wind1 = wind1;
+    }
+
+    public String getWind2() {
+        return wind2;
+    }
+
+    public void setWind2(String wind2) {
+        this.wind2 = wind2;
+    }
+
+    public String getWind3() {
+        return wind3;
+    }
+
+    public void setWind3(String wind3) {
+        this.wind3 = wind3;
+    }
+
+    public String getWind4() {
+        return wind4;
+    }
+
+    public void setWind4(String wind4) {
+        this.wind4 = wind4;
+    }
+
+    public String getWind5() {
+        return wind5;
+    }
+
+    public void setWind5(String wind5) {
+        this.wind5 = wind5;
+    }
+
+    public String getWind6() {
+        return wind6;
+    }
+
+    public void setWind6(String wind6) {
+        this.wind6 = wind6;
+    }
+
+    public String getFl1() {
+        return fl1;
+    }
+
+    public void setFl1(String fl1) {
+        this.fl1 = fl1;
+    }
+
+    public String getFl2() {
+        return fl2;
+    }
+
+    public void setFl2(String fl2) {
+        this.fl2 = fl2;
+    }
+
+    public String getFl3() {
+        return fl3;
+    }
+
+    public void setFl3(String fl3) {
+        this.fl3 = fl3;
+    }
+
+    public String getFl4() {
+        return fl4;
+    }
+
+    public void setFl4(String fl4) {
+        this.fl4 = fl4;
+    }
+
+    public String getFl5() {
+        return fl5;
+    }
+
+    public void setFl5(String fl5) {
+        this.fl5 = fl5;
+    }
+
+    public String getFl6() {
+        return fl6;
+    }
+
+    public void setFl6(String fl6) {
+        this.fl6 = fl6;
+    }
+
+    public String getIndex_d() {
+        return index_d;
+    }
+
+    public void setIndex_d(String index_d) {
+        this.index_d = index_d;
+    }
+
+    public String getIndex48_d() {
+        return index48_d;
+    }
+
+    public void setIndex48_d(String index48_d) {
+        this.index48_d = index48_d;
+    }
+
+    public String getIndex_uv() {
+        return index_uv;
+    }
+
+    public void setIndex_uv(String index_uv) {
+        this.index_uv = index_uv;
+    }
+
+    public String getIndex48_uv() {
+        return index48_uv;
+    }
+
+    public void setIndex48_uv(String index48_uv) {
+        this.index48_uv = index48_uv;
+    }
+
+}
---
+++ src/main/java/com/bruce/weixin/bean/WeatherWrapper.java
@@ -0,0 +1,18 @@
+package com.bruce.weixin.bean;
+
+public class WeatherWrapper {
+    
+    private WeatherInfo weatherinfo;
+
+    public WeatherInfo getWeatherinfo() {
+        return weatherinfo;
+    }
+
+    public void setWeatherinfo(WeatherInfo weatherinfo) {
+        this.weatherinfo = weatherinfo;
+    }
+
+    
+    
+}
+
